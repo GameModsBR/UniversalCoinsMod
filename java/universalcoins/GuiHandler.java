d2bff14751ed934374df623bec03f2c4439aa4ec
joserobjr <jose.rob.jr@gmail.com>
2015-12-13 21:32:46 -0300

    Added IDs to GUIs

--- java/universalcoins/GuiHandler.java
+++ java/universalcoins/GuiHandler.java
@@ -42,10 +42,77 @@ import universalcoins.tile.TileUCSign;
 import universalcoins.tile.TileVendor;
 
 class GuiHandler implements IGuiHandler {
+	public static final int GUI_TRADE_STATION = 1,
+							GUI_VENDOR_WRENCH = 2,
+							GUI_VENDOR_OWNER = 3,
+							GUI_VENDOR_SELL = 4,
+							GUI_VENDOR_BUY = 5,
+							GUI_CARD_STATION = 6,
+							GUI_SAFE = 7,
+							GUI_BANDIT = 9,
+							GUI_BANDIT_WRENCH = 10,
+							GUI_SIGNAL = 11,
+							GUI_PACKAGER = 12,
+							GUI_POWER_BASE = 13,
+							GUI_POWER_RECEIVER = 14;
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		TileEntity tileEntity = world.getTileEntity(x, y, z);
+
+		switch (ID) {
+			case GUI_TRADE_STATION:
+				if(tileEntity instanceof TileTradeStation)
+					return new ContainerTradeStation(player.inventory, (TileTradeStation)tileEntity);
+				else return null;
+			case GUI_VENDOR_WRENCH:
+			case GUI_VENDOR_OWNER:
+			case GUI_VENDOR_SELL:
+			case GUI_VENDOR_BUY:
+				if(tileEntity instanceof TileVendor) {
+					TileVendor tile = (TileVendor) tileEntity;
+					switch (ID){
+						case GUI_VENDOR_WRENCH: return new ContainerVendorWrench(player.inventory, tile);
+						case GUI_VENDOR_OWNER: return new ContainerVendor(player.inventory, tile);
+						case GUI_VENDOR_SELL: return new ContainerVendorSell(player.inventory, tile);
+						default: return new ContainerVendorBuy(player.inventory, tile);
+					}
+				}
+				else return null;
+			case GUI_CARD_STATION:
+				if(tileEntity instanceof TileCardStation)
+					return new ContainerCardStation(player.inventory, (TileCardStation)tileEntity);
+				else return null;
+			case GUI_SAFE:
+				if(tileEntity instanceof TileSafe)
+					return new ContainerSafe(player.inventory, (TileSafe)tileEntity);
+				else return null;
+			case GUI_BANDIT:
+			case GUI_BANDIT_WRENCH:
+				if(tileEntity instanceof TileBandit) {
+					TileBandit tile = (TileBandit) tileEntity;
+					if(ID == GUI_BANDIT) return new ContainerBandit(player.inventory, tile);
+					else return null;
+				}
+				else return null;
+			case GUI_SIGNAL:
+				if(tileEntity instanceof TileSignal)
+					return new ContainerSignal(player.inventory, (TileSignal) tileEntity);
+				else return null;
+			case GUI_PACKAGER:
+				if(tileEntity instanceof TilePackager)
+					return new ContainerPackager(player.inventory, (TilePackager) tileEntity);
+				else return null;
+			case GUI_POWER_BASE:
+				if(tileEntity instanceof TilePowerBase)
+					return new ContainerPowerBase(player.inventory, (TilePowerBase) tileEntity);
+				else return null;
+			case GUI_POWER_RECEIVER:
+				if(tileEntity instanceof TilePowerReceiver)
+					return new ContainerPowerReceiver(player.inventory, (TilePowerReceiver) tileEntity);
+				else return null;
+		}
+
 		if (tileEntity instanceof TileTradeStation) {
 			return new ContainerTradeStation(player.inventory, (TileTradeStation) tileEntity);
 		}
@@ -94,6 +161,60 @@ class GuiHandler implements IGuiHandler {
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		TileEntity tileEntity = world.getTileEntity(x, y, z);
+
+		switch (ID) {
+			case GUI_TRADE_STATION:
+				if(tileEntity instanceof TileTradeStation)
+					return new TradeStationGUI(player.inventory, (TileTradeStation)tileEntity);
+				else return null;
+			case GUI_VENDOR_WRENCH:
+			case GUI_VENDOR_OWNER:
+			case GUI_VENDOR_SELL:
+			case GUI_VENDOR_BUY:
+				if(tileEntity instanceof TileVendor) {
+					TileVendor tile = (TileVendor) tileEntity;
+					switch (ID){
+						case GUI_VENDOR_WRENCH: return new VendorWrenchGUI(player.inventory, tile);
+						case GUI_VENDOR_OWNER: return new VendorGUI(player.inventory, tile);
+						case GUI_VENDOR_SELL: return new VendorSellGUI(player.inventory, tile);
+						default: return new VendorBuyGUI(player.inventory, tile);
+					}
+				}
+				else return null;
+			case GUI_CARD_STATION:
+				if(tileEntity instanceof TileCardStation)
+					return new CardStationGUI(player.inventory, (TileCardStation)tileEntity);
+				else return null;
+			case GUI_SAFE:
+				if(tileEntity instanceof TileSafe)
+					return new SafeGUI(player.inventory, (TileSafe)tileEntity);
+				else return null;
+			case GUI_BANDIT:
+			case GUI_BANDIT_WRENCH:
+				if(tileEntity instanceof TileBandit) {
+					TileBandit tile = (TileBandit) tileEntity;
+					if(ID == GUI_BANDIT) return new BanditGUI(player.inventory, tile);
+					else return null;
+				}
+				else return null;
+			case GUI_SIGNAL:
+				if(tileEntity instanceof TileSignal)
+					return new SignalGUI(player.inventory, (TileSignal) tileEntity);
+				else return null;
+			case GUI_PACKAGER:
+				if(tileEntity instanceof TilePackager)
+					return new PackagerGUI(player.inventory, (TilePackager) tileEntity);
+				else return null;
+			case GUI_POWER_BASE:
+				if(tileEntity instanceof TilePowerBase)
+					return new PowerBaseGUI(player.inventory, (TilePowerBase) tileEntity);
+				else return null;
+			case GUI_POWER_RECEIVER:
+				if(tileEntity instanceof TilePowerReceiver)
+					return new PowerReceiverGUI(player.inventory, (TilePowerReceiver) tileEntity);
+				else return null;
+		}
+
 		if (tileEntity instanceof TileTradeStation) {
 			return new TradeStationGUI(player.inventory, (TileTradeStation) tileEntity);
 		}
