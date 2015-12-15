4bf586e3aec7c40dc6c9f36aa2ef60b4dbd4fc9a
joserobjr <jose.rob.jr@gmail.com>
2015-12-15 12:04:08 -0300

    Added support for json lines

--- java/universalcoins/net/UCTileSignMessage.java
+++ java/universalcoins/net/UCTileSignMessage.java
@@ -5,8 +5,11 @@ import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IChatComponent;
 import universalcoins.tile.TileUCSign;
 
 public class UCTileSignMessage implements IMessage, IMessageHandler<UCTileSignMessage, IMessage> {
@@ -55,13 +58,37 @@ public class UCTileSignMessage implements IMessage, IMessageHandler<UCTileSignMe
 		ByteBufUtils.writeUTF8String(buf, this.blockIcon);
 	}
 
+	@SideOnly(Side.CLIENT)
+	public String[] getTranslatedText()
+	{
+		String[] lines = new String[4];
+		for(int i = 0; i < 4; i++)
+		{
+			String str = signText[i];
+			if(!str.isEmpty() && str.charAt(0) == '{')
+				try
+				{
+					IChatComponent component = IChatComponent.Serializer.func_150699_a(str);
+					str = component.getFormattedText();
+					str = str.replaceFirst("^\u00A7([0-9a-fl-r])", "&\\1");
+					str = str.replaceAll("\u00A7[0-9a-fl-r]", "");
+				}
+				catch (Exception ignored)
+				{}
+
+			lines[i] = str;
+		}
+
+		return lines;
+	}
+
 	@Override
 	public IMessage onMessage(UCTileSignMessage message, MessageContext ctx) {
 		TileEntity tileEntity = FMLClientHandler.instance().getClient().theWorld.getTileEntity(message.xCoord,
 				message.yCoord, message.zCoord);
 
 		if (tileEntity != null && tileEntity instanceof TileUCSign) {
-			((TileUCSign) tileEntity).signText = message.signText;
+			((TileUCSign) tileEntity).signText = message.getTranslatedText();
 			((TileUCSign) tileEntity).blockOwner = message.blockOwner;
 			((TileUCSign) tileEntity).blockIcon = message.blockIcon;
 		}
